2ee5343579ef8940a1cd5f1aa898aadb74432cc7
Audrey <audreyannagoddard@gmail.com>
2025-12-06 20:45:53 -0500

    Position recipe modal much higher - 100px above subtitle

--- my-app/src/components/MealPlanGenerator.jsx
+++ my-app/src/components/MealPlanGenerator.jsx
@@ -577,9 +577,9 @@ function MealPlanGenerator({ showBrowseOnly = false, onRecipeClick }) {
           const rect = sectionTitle.getBoundingClientRect();
           const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
           
-          // Position modal even higher - above the category title
+          // Position modal much higher - well above the category title
           setRecipeSectionPosition({
-            top: rect.top + scrollTop - 10, // Position even higher, slightly above subtitle
+            top: rect.top + scrollTop - 100, // Position much higher above subtitle
             left: 0 // Will be centered using transform: translateX(-50%)
           });
         } else {
